b0f20b6d6167f3fb0b02042d04e3a9381b6963d0
jinparksj <jinparksj@gmail.com>
2020-01-15 00:15:12 -0800

    semaphore

--- sketch_jan14a/sketch_jan14a.ino
+++ sketch_jan14a/sketch_jan14a.ino
@@ -4,7 +4,7 @@
 #define BUF_SIZE 16
 int buf[BUF_SIZE] = {0, };
 int front = 0;
-int reat = 0;
+int rear = 0;
 int how_many = 0;
 SemaphoreHandle_t button_pressed = NULL;
 
@@ -12,10 +12,83 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
+  attachInterrupt(digitalPinToInterrupt(2), buttonIsr, RISING);
+
+  button_pressed = xSemaphoreCreateCounting(BUF_SIZE, 0);
+
+  xTaskCreate(tbutton_main, NULL, configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
+  vTaskStartScheduler();
+
+  while(true);
+
+}
+
+void buttonIsr(void) {
+  static int DevRegister = 0;
+  int DataToSend = 0;
+
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+  DevRegister++;
+
+  DataToSend = DevRegister;
+
+  Serial.print(" isr( ");
+  Serial.print(DataToSend);
+  Serial.print(" ) => ");
+
+  if (how_many < BUF_SIZE) {
+    buf[rear] = DataToSend;
+    rear++;
+    if (rear >= BUF_SIZE) {
+      rear = 0;
+    }
+    how_many++;
+  }
+
+  xSemaphoreGiveFromISR(button_pressed, &xHigherPriorityTaskWoken);
+
+  if (xHigherPriorityTaskWoken) {
+    taskYIELD();
+  }
+}
+
+void tbutton_main(void *arg) {
+  int DataToReceive = 0;
+  const TickType_t xBlockTime = pdMS_TO_TICKS(200);
+
+  while (true) {
+    if (xSemaphoreTake(button_pressed, xBlockTime) == pdTRUE) {
+      noInterrupts();
+      if (how_many > 0) {
+        DataToReceive = buf[front];
+        front++;
+        if (front >= BUF_SIZE) {
+          front = 0;
+        }
+        how_many--;
+      }
+      interrupts();
+
+      noInterrupts();
+
+      Serial.print(" tbutton( ");
+      Serial.print(DataToReceive);
+      Serial.print(" ) ");
+      Serial.println();
+      interrupts();
+    }
+    
+  }
+
   
 }
 
+
+
 void loop() {
   // put your main code here, to run repeatedly:
+
 
 }
