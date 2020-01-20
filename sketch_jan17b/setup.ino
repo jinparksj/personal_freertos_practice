d3050a809b031f8e7cca3afd834524a8c850f58b
jinparksj <jinparksj@gmail.com>
2020-01-20 01:11:13 -0800

    update to lenovo

--- sketch_jan17b/setup.ino
+++ sketch_jan17b/setup.ino
@@ -21,6 +21,26 @@ void handle_OnConnect() {
 
 String SendHTML(bool led1_on, bool led2_on) {
   String ptr = "<!DOCTYPE html> <html>\n";
-  ptr += "<head><meta name\"
+  ptr += "<head><meta name\"viewport\" ";
+  ptr += "content=\"width=device-width, ";
+  ptr += "initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>LED Control<\title>\n";
+  ptr += "<style>html { font-family: Helvetica; ";
+  ptr += "display: inline-block; margin: 0px auto; ";
+  ptr += "text-aligh: center;}\n";
+  ptr += "body{margin-top";
+  ptr += "";
+  ptr += "";
+  ptr += "";
+  ptr += "";
+  ptr += "";
+  ptr += "";
+  ptr += "";
+  ptr += "";
+  ptr += "";
+  ptr += "";
+  
+  ptr += "";
+  
   
 }
