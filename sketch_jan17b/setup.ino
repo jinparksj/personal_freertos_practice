5c897c37bff13ff639643ff0de039d1ec4e96122
jinparksj <jinparksj@gmail.com>
2020-01-20 14:09:29 -0800

    update from linux

--- sketch_jan17b/setup.ino
+++ sketch_jan17b/setup.ino
@@ -19,28 +19,98 @@ void handle_OnConnect() {
 
 }
 
+void handle_led1on() {
+  ledESP12_ON = true;
+  Serial.println("GPIO16 Status: ON");
+
+  String html = SendHTML(ledESP12_ON, ledNodeMCU_ON);
+  webServer.send(200, "text/html", html);
+}
+
+
+void handle_led1off() {
+  ledESP12_ON = false;
+  Serial.println("GPIO16 Status: OFF");
+
+  String html = SendHTML(ledESP12_ON, ledNodeMCU_ON);
+  webServer.send(200, "text/html", html);
+}
+
+
+void handle_led2on() {
+  ledNodeMCU_ON = true;
+  Serial.println("GPIO2 Status: ON");
+
+  String html = SendHTML(ledESP12_ON, ledNodeMCU_ON);
+  webServer.send(200, "text/html", html);
+}
+
+
+void handle_led2off() {
+  ledNodeMCU_ON = false;
+  Serial.println("GPIO2 Status: OFF");
+
+  String html = SendHTML(ledESP12_ON, ledNodeMCU_ON);
+  webServer.send(200, "text/html", html);
+}
+
+
+void handle_NotFound() {
+  webServer.send(404, "text/plain", "Not Found");
+}
 String SendHTML(bool led1_on, bool led2_on) {
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr += "<head><meta name\"viewport\" ";
   ptr += "content=\"width=device-width, ";
   ptr += "initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>LED Control<\title>\n";
+  ptr += "<title>LED Control</title>\n";
   ptr += "<style>html { font-family: Helvetica; ";
   ptr += "display: inline-block; margin: 0px auto; ";
-  ptr += "text-aligh: center;}\n";
-  ptr += "body{margin-top";
-  ptr += "";
-  ptr += "";
-  ptr += "";
-  ptr += "";
-  ptr += "";
-  ptr += "";
-  ptr += "";
-  ptr += "";
-  ptr += "";
-  ptr += "";
-  
-  ptr += "";
-  
-  
+  ptr += "text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;";
+  ptr += "margin: 50px auto 30px;} h3 {color: #444444;";
+  ptr += "margin-bottom: 50px;}\n";
+  ptr += ".button {display: block; width: 80px;";
+  ptr += "background-color: #1abc9c;border: none;";
+  ptr += "color: white; padding:13px 30px;";
+  ptr += "text-decoration: none; font-size: 25px;";
+  ptr += "margin: 0px auto 35px;cursor: pointer;";
+  ptr += "border-radius: 4px;}\n";
+  ptr += ".button-on {background-color: #1abc9c;}\n";
+  ptr += ".button-on:active {background-color: #16a085;}\n";
+  ptr += ".button-off {background-color: #34495e;}\n";
+  ptr += ".button-off:active {background-color: #2c3e50;}\n";
+  ptr += "p {font-size: 14px; color: #888;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<h1>NodeMCU Web Server</h1>\n";
+
+  if (led1_on) {
+    ptr += "<p>LED1 Status: On</p>";
+    ptr += "<a class=\"button button-off\"";
+    ptr += "href=\"/led1off\">OFF</a>\n";
+  }
+  else {
+    ptr += "<p>LED1 Status: OFF</p>";
+    ptr += "<a class=\"button button-on\"";
+    ptr += "href=\"/led1on\">ON</a>\n";
+  }
+
+  if (led2_on) {
+    ptr += "<p>LED2 Status: On</p>";
+    ptr += "<a class=\"button button-off\"";
+    ptr += "href=\"/led2off\">OFF</a>\n";
+  }
+  else {
+    ptr += "<p>LED2 Status: OFF</p>";
+    ptr += "<a class=\"button button-on\"";
+    ptr += "href=\"/led2on\">ON</a>\n";
+  }
+
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+
+  return ptr;
+
 }
--- sketch_jan17b/sketch_jan17b.ino
+++ sketch_jan17b/sketch_jan17b.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid = "Synology";
-const char* passwd = "Goecoli321";
+const char* ssid = "TP-LINK_4CEF";//"Synology";
+const char* passwd = "25529029";//"Goecoli321";
 
 const int HTTP_PORT = 80;
 ESP8266WebServer webServer(HTTP_PORT);
@@ -11,7 +11,7 @@ uint8_t ledESP12 = D0; //16
 uint8_t ledNodeMCU = D4; //12
 
 const int LED_ON = LOW;
-const int LEF_OFF = HIGH;
+const int LED_OFF = HIGH;
 bool ledESP12_ON = false;
 bool ledNodeMCU_ON = false;
 
@@ -19,7 +19,9 @@ void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(115200);
-  while (!Serial);
+  //while (!Serial || (millis() < 10000));
+  Serial.println("Serial Done");
+  
   pinMode(ledESP12, OUTPUT);
   pinMode(ledNodeMCU, OUTPUT);
 
@@ -39,9 +41,35 @@ void setup() {
 
   //HTTP Request Function
   webServer.on("/", handle_OnConnect);
+  webServer.on("/led1on", handle_led1on);
+  webServer.on("/led1off", handle_led1off);
+  webServer.on("/led2on", handle_led2on);
+  webServer.on("/led2off", handle_led2off);
+  webServer.onNotFound(handle_NotFound);
+
+  webServer.begin();
+
+  Serial.print("HTTP server ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println(" ' to connect");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  webServer.handleClient();
 
+  if (ledESP12_ON) {
+    digitalWrite(ledESP12, LED_ON);
+  }
+  else {
+    digitalWrite(ledESP12, LED_OFF);
+  }
+
+  if (ledNodeMCU_ON) {
+    digitalWrite(ledNodeMCU, LED_ON);
+  }
+  else {
+    digitalWrite(ledNodeMCU, LED_OFF);
+  }
 }
